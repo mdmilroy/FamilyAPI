726c0c6977db43ef16728ba388decd46efdfd50d
Michael Milroy <mdmilroy@gmail.com>
2020-04-18 14:59:54 -0400

    removed family, built up person, get list of people

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -16,6 +16,7 @@ using Microsoft.Owin.Security.OAuth;
 using API.Models;
 using API.Providers;
 using API.Results;
+using Data;
 
 namespace API.Controllers
 {
---
+++ Models/Entities/FamilyListPeople.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class FamilyListPeople
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Birthday")]
+        public DateTime Birthdate { get; set; }
+    }
+}
